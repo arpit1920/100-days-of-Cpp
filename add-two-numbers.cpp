e5c8b4a56378aadb8021e6f5233940cf5ed4849e
Arpit Bhushan Sharma <42774305+arpit1920@users.noreply.github.com>
2020-09-16 16:10:39 +0530

    Add files via upload

---
+++ add-two-numbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream> 
+int addTwoNumber(int A, int B) 
+{
+	return A + B;
+} 
+ 
+int main()
+{ 
+	int A = 2, B = 3;
+	std::cout << "sum = " << addTwoNumber(A, B); 
+	return 0; 
+} 
+
---
+++ welcome message.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string name;
+	cout<<"May I Know Your Name Please";
+	getline(cin,name);
+	cout<<"Welcome Mr."<<name;
+	return 0;
+}
