aa9febac5eaeff5836c32ec298a95bdd9513ad20
Arpit Bhushan Sharma <42774305+arpit1920@users.noreply.github.com>
2020-10-02 00:20:51 +0530

    Add files via upload

---
+++ swapValues.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, temp;
+    cout<<"Enter the value of a and b";
+    cin>>a>>b;
+    cout << "Before swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+    temp = a;
+    a = b;
+    b = temp;
+    cout << "\nAfter swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+    return 0;
+}
