dfc5c405a3ded3019e1085e44428173b475a93b4
Arpit Bhushan Sharma <42774305+arpit1920@users.noreply.github.com>
2020-10-26 00:04:50 +0530

    Add files via upload

---
+++ 2Darray.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+	int A[2][3] = {{2,4,6},{3,6,9}};
+	int i,j;
+	for(i=0;i<2; i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			cout<<A[i][j];
+		}
+		cout<<endl;
+	}
+	return 0;
+}
